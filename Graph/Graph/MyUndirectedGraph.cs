d0a3bbd1b01694ffe8733c6d9cd7a4ae90a1b984
ivu-u <123986386+IvTwo@users.noreply.github.com>
2023-03-22 12:18:08 -0400

    class setup

---
+++ Graph/Graph/MyUndirectedGraph.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    internal class MyUndirectedGraph
+    {
+        private List<Node> nodes;
+        private int[,] edges;  // adjacency matrix
+
+        public MyUndirectedGraph(int size)
+        {
+            nodes = new List<Node>();
+            edges = new int[size, size];    // default value == 0
+        }
+    }
+}
---
+++ Graph/Graph/Node.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    internal class Node
+    {
+        private string data;
+        private List<Node> connections;
+
+        public Node(string d)
+        {
+            data = d;
+            connections = new List<Node>();
+        }
+
+        // returns true if a node(parameter) is adjacent to this node
+        public bool CheckAdjacency(Node node)
+        {
+            if (connections.Contains(node))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
