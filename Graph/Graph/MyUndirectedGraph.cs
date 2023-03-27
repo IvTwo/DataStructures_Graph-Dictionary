9fa94e413e5f08f310805e9f277bd5675fa7ff9e
ivu-u <123986386+IvTwo@users.noreply.github.com>
2023-03-27 13:52:45 -0400

    check adjacency implemented

--- Graph/Graph/MyUndirectedGraph.cs
+++ Graph/Graph/MyUndirectedGraph.cs
@@ -19,7 +19,6 @@ namespace Graph
             edges = new int[s, s];    // default value == 0
         }
 
-        // generate a random UndirectedGraph of "size"
         public void RandomGraph()
         {
             // generate "size" nodes and add them to nodes(list)
@@ -35,24 +34,95 @@ namespace Graph
                 {
                     var rand = new Random();
 
-                    if (rand.Next(0,2) == 1)
+                    if (rand.Next(0,14) == 1)    // how many connections are made
                     {
                         edges[i, j] = 1;
+                        edges[j, i] = 1;
                     }
                 }
             }
         }
 
-        public void AdjacencyMatrix()
+        public void PrintAdjacencyMatrix()
         {
             for (int i = 0; i < edges.GetLength(0); i++)
             {
+                Console.Write((i+1).ToString().PadRight(3) + "| ");
+
                 for (int j = 0; j < edges.GetLength(1); j++)
                 {
                     Console.Write(edges[i, j] + "   ");
                 }
+
+                if (i == edges.GetLength(0) - 1)
+                {
+                    Console.WriteLine();
+                    Console.Write("   | ");
+                    Console.Write("".PadRight(edges.GetLength(1)*4, '-'));
+                    Console.WriteLine();
+                    Console.Write("     ");
+                    for (int k = 0; k < edges.GetLength(1); k++)
+                    {
+                        Console.Write((k+1).ToString().PadRight(4));
+                    }
+                }
                 Console.WriteLine();
             }
+        }
+
+        // check if there is  path from node1 to node2
+        // ref: https://www.koderdojo.com/blog/depth-first-search-algorithm-in-csharp-and-net-core
+        public bool CheckPath(int startNode, int endNode)
+        {
+            HashSet<int> visited = new HashSet<int>();  // set of all nodes already visited
+
+            // check if input is valid
+            if (startNode > size || endNode > size || startNode < 0 || endNode< 0)
+            {
+                return false;
+            }
+
+            Stack<int> stack = new Stack<int>();
+            stack.Push(startNode);
+
+            // "stack" is all nodes connected to the start node.
+            // once there are no nodes to check exit the while loop
+            while (stack.Count > 0)
+            {
+                int vertex = stack.Pop();   // set top of the stack to vertex (node we arae checking)
+
+                // if the node has already been visited, exit this loop iteration
+                if (visited.Contains(vertex))
+                    continue;
+                
+                // otherwise add node to "visited"
+                visited.Add(vertex);
+
+                // iterate through adjacency matrix for startNode
+                for(int i = 0; i < edges.GetLength(1); i++)
+                {
+                    // for all adjacent nodes that we have not visited yet --> add to stack
+                    if (edges[vertex, i] == 1 && !visited.Contains(i))
+                    {
+                        stack.Push(i);
+                    }
+                }
+            }
+
+            // if endNode is in the set "visited" then there is a valid path from startNode to endNode --> return true
+            if (visited.Contains(endNode))
+                return true;
+            else
+                return false;
+        }
+
+        // return true if node1 & node2 are directly adjacent
+        public bool CheckAdjacency(int node1, int node2)
+        {
+            if (edges[node1, node2] == 1)
+                return true;
+            else
+                return false;
         }
 
         public void AddEdge()
--- Graph/Graph/Program.cs
+++ Graph/Graph/Program.cs
@@ -4,17 +4,19 @@
     {
         static void Main(string[] args)
         {
-            int size = 20;  // number of nodes in the graph
-
-            // create UndirectedGraph
-            MyUndirectedGraph graph = new MyUndirectedGraph(size);
-            graph.RandomGraph();
-            graph.AdjacencyMatrix();
+            MyMenu();
         }
 
         // accept user input as a string
         public static void MyMenu()
         {
+            int size = 20;  // number of nodes in the graph
+
+            // create UndirectedGraph
+            MyUndirectedGraph graph = new MyUndirectedGraph(size);
+            graph.RandomGraph();
+
+
             bool keepGoing = true;
             Console.WriteLine();
             Console.WriteLine("Welcome To The Undirected Graph Showcase----!");
@@ -28,21 +30,47 @@
 
                 switch (userInput)   // handle user input
                 {
-                    case "1":
-                        
+                    case "1":   // print adjacency matrix
+                        Console.WriteLine();
+                        Console.WriteLine("Adjacency Matrix of the current graph---");
+                        Console.WriteLine();
+                        graph.PrintAdjacencyMatrix();
                         break;
 
-                    case "2":
-                        
+                    case "2":   // check if a path exists
+                        Console.WriteLine();
+                        Console.WriteLine("Check if there is a path from node1 to node2");
+                        string[] words = UserInput().Split(' ');
+
+                        // if the user inputs more/less than 2 nodes
+                        if (words.Length != 2) 
+                        {
+                            Console.WriteLine("---");
+                            Console.WriteLine("Invalid Input!");
+                            Console.WriteLine("returning to menu input...");
+                            break;
+                        }
+
+                        if (graph.CheckPath(int.Parse(words[0])-1, int.Parse(words[1])-1))
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Yes: there is a path between " + int.Parse(words[0]) + " & " + int.Parse(words[1]));
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("No: path not found between " + int.Parse(words[0]) + " & " + int.Parse(words[1]));
+                        }
                         break;
 
                     case "3":
-
+                        keepGoing = false;
                         break;
 
                     default:
                         Console.WriteLine("---");
                         Console.WriteLine("Invalid Input!");
+                        Console.WriteLine("returning to menu input...");
                         break;
                 }
             }
