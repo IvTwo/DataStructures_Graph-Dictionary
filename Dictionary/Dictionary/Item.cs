70acfa1dcd287340e22d824c03ab804f4518d2d3
ivu-u <123986386+IvTwo@users.noreply.github.com>
2023-03-29 12:03:21 -0400

    generate and print player inventory

---
+++ Dictionary/Dictionary/Item.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Item
+    {
+        private string name;
+        private int power;
+        private int price;
+        private int quantity;
+
+        public Item(string name, int power, int price)  // constructor
+        {
+            this.name = name;
+            this.power = power;
+            this.price = price;
+            this.quantity = 1;
+        }
+
+        // getters
+        public string GetName()
+        {
+            return name;
+        }
+        public int GetPrice()
+        {
+            return price;
+        }
+        public int GetPower()
+        {
+            return power;
+        }
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+
+        public void IncreaseQuantity()
+        {
+            quantity++;
+        }
+    }
+}
--- Dictionary/Dictionary/Program.cs
+++ Dictionary/Dictionary/Program.cs
@@ -4,7 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            userInterface();
         }
+
+        public static void userInterface()
+        {
+            Player player = new Player(100, 10);
+
+            Console.WriteLine();
+            Console.WriteLine("Welcome to the Inventory Simulator----!");
+            Console.WriteLine();
+            Console.WriteLine("\t1. Buy\n" + "\t2. Sell\n" + "\t3. Inventory\n" +
+                                "\t4. Collection\n" + "\t5. Quit\n");
+
+            bool keepGoing = true;
+            while (keepGoing) 
+            {
+                switch (UserInput()) // handle user input
+                {
+                    case "1":
+                        break;
+
+                    case "2":
+                        break;
+
+                    case "3":
+                        player.DisplayInventory();
+                        break;
+
+                    case "4":
+                        break;
+
+                    case "5":
+                        keepGoing = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("---");
+                        Console.WriteLine("Invalid Input!");
+                        break;
+                }
+            }
+        }
+
+        // return the user input
+        public static string UserInput()
+        {
+            Console.Write("\t * Your Input: ");
+            return Console.ReadLine();
+        }
+
     }
 }
